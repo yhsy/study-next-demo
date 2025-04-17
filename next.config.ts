f287380c175cdcc518526cbc30648b947316048d
juerui <juerui@vip.qq.com>
2025-04-17 15:42:04 +0800

    canary版回退正常版

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // 部分预渲染
-  experimental: {
-    ppr: 'incremental' // 允许您为特定路由采用 PPR。
-  }
+  // 部分预渲染(实验功能,canary版才支持:next@canary)
+  // experimental: {
+  //   ppr: 'incremental' // 允许您为特定路由采用 PPR。
+  // }
 };
 
 export default nextConfig;
