df0fe84bb7cf5cedb8a75b3eda9f5459fb89ad1b
juerui <juerui@vip.qq.com>
2025-04-17 16:47:25 +0800

    表格-基础

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,31 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+
+// 防抖
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  // 获取搜索参数
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  // 搜索参数变化监听(加防抖)
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(term);
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    // 路由地址跳转
+    replace(`${pathname}?${params.toString()}`);
+  },300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +34,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   // 部分预渲染(实验功能,canary版才支持:next@canary)
   // experimental: {
   //   ppr: 'incremental' // 允许您为特定路由采用 PPR。
