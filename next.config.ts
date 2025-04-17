f5c98eddca1cd381386a2b5900c25286d99dfa96
juerui <juerui@vip.qq.com>
2025-04-17 15:31:00 +0800

    canary版支持的部分预渲染功能；

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 部分预渲染
+  experimental: {
+    ppr: 'incremental' // 允许您为特定路由采用 PPR。
+  }
 };
 
 export default nextConfig;
