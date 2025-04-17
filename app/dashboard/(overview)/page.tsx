855b63e39d63c0e5d92e86418e1e0eef197a547a
juerui <juerui@vip.qq.com>
2025-04-17 14:28:20 +0800

    流式处理：LatestInvoices

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,14 +3,15 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
-import { fetchLatestInvoices,fetchCardData } from '@/app/lib/data';
+// import { fetchRevenue,fetchLatestInvoices,fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton,LatestInvoicesSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const latestInvoices = await fetchLatestInvoices();
   const cardData = await fetchCardData();
   const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = cardData;
 
@@ -33,10 +34,14 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         {/* <RevenueChart revenue={revenue} /> */}
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -62,6 +62,8 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
+    // await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
