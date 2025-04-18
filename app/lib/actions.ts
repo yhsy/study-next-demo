dd97baabed37e2eec8211f4ef886460aa00c49b5
juerui <juerui@vip.qq.com>
2025-04-18 11:05:52 +0800

    发票-删除

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -96,3 +96,9 @@ export async function updateInvoice(id: string, formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  // 触发新的服务器请求
+  revalidatePath('/dashboard/invoices');
+}
