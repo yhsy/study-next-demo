a86962307ea9d13876a7ba0c5d52a55253a2aee8
juerui <juerui@vip.qq.com>
2025-04-18 16:56:26 +0800

    表单校验-改成actions去校验

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -32,25 +32,52 @@ const sql = getDB();
 
 // 定义发票表单的验证Schema
 const FormSchema = z.object({
-  id: z.string(),                              // 发票ID，字符串类型
-  customerId: z.string(),                      // 客户ID，字符串类型
-  amount: z.coerce.number(),                   // 金额，强制转换为数字类型
-  status: z.enum(['pending', 'paid']),         // 状态，限定为'pending'或'paid'
-  date: z.string(),                            // 日期，字符串类型
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
+  date: z.string(),
 });
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 // 创建新发票时使用的Schema，省略id和date字段
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 // 创建新发票的服务端操作函数
-export async function createInvoice(formData: FormData) {
-  // 解析并验证表单数据
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  // // 解析并验证表单数据
+  // const { customerId, amount, status } = CreateInvoice.parse({
+  //   customerId: formData.get('customerId'),
+  //   amount: formData.get('amount'),
+  //   status: formData.get('status'),
+  // });
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
 
+  const { customerId, amount, status } = validatedFields.data;
   // 将金额转换为分为单位
   const amountInCents = amount * 100;
   // 获取当前日期（YYYY-MM-DD格式）
