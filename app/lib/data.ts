4b683d7cf0d64b512b350ef139d9a59b44a0d73f
juerui <juerui@vip.qq.com>
2025-04-18 13:43:51 +0800

    not-found页(编辑页-错误id)

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -237,11 +237,10 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
+    // throw new Error('Failed to fetch invoice.');
   }
 }
 
