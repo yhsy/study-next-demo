582675f4a7cbf00ef994effbbf5ecc98e89b6206
juerui <juerui@vip.qq.com>
2025-04-18 17:04:59 +0800

    编辑-表单验证（服务端）

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -99,13 +99,30 @@ export async function createInvoice(prevState: State, formData: FormData) {
 // 编辑发票
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+// export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice( 
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  // const { customerId, amount, status } = UpdateInvoice.parse({
+  //   customerId: formData.get('customerId'),
+  //   amount: formData.get('amount'),
+  //   status: formData.get('status'),
+  // });
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-
+ 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   try {
     await sql`
